cfeb203330f36299c657047a563b80f0c8c5dd25
MMAndresC <mandrescasabon@hotmail.com>
2022-05-17 12:51:13 +0200

    Iteracion#2

--- loops.js
+++ loops.js
@@ -6,3 +6,22 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
 for(let product of products){
     product.includes('Camiseta') ? console.log(product) : '';
 }
+
+//Iteracion#2
+//Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad 
+//isApproved a true o false en consecuencia. Una vez lo tengas compruébalo con un console.log.
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+for(let alumn of alumns){
+    alumn.isApproved = false;
+    if((alumn.T1 && alumn.T2) || (alumn.T2 && alumn.T3) || (alumn.T1 && alumn.T3)){
+        alumn.isApproved = true;
+    }
+}
+console.log(alumns);
